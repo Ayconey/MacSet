2db1ad56652042ad3a9886ad4bac103daa9f245f
Aycone <ayconey@gmail.com>
2022-09-12 15:41:10 +0200

    changed url for cloud

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -20,14 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-st228_u-3uvsn%s&^l_c%8s=r%kx&##s0s498-+_1$nc%jn4q4'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG',0)))
 
-ALLOWED_HOSTS = ["node85731-compose.unicloud.pl","172.104.147.246","127.0.0.1","localhost"]
-
-
+#ALLOWED_HOSTS = filter(None,os.environ.get("ALLOWED_HOSTS",'').split(','))
+ALLOWED_HOSTS = ['node85731-compose.unicloud.pl','139.162.147.158','127.0.0.1','localhost']
 # Application definition
 
 INSTALLED_APPS = [
@@ -79,26 +78,17 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
-#         "NAME": os.environ.get("SQL_DATABASE", BASE_DIR / "db.sqlite3"),
-#         "USER": os.environ.get("SQL_USER", "user"),
-#         "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
-#         "HOST": os.environ.get("SQL_HOST", "localhost"),
-#         "PORT": os.environ.get("SQL_PORT", "5432"),
-#     }
-# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mcSet',
-        'USER':'mcSet',
-        'PASSWORD':'mysecretpassword',
-        'HOST':"db",
-        'PORT':'5432',
+    "default": {
+        "ENGINE": 'django.db.backends.postgresql',
+        "NAME": os.environ.get("DB_NAME",'mcSet'),
+        "USER": os.environ.get("DB_USER", "user"),
+        "PASSWORD": os.environ.get("DB_PASS", "mysecretpassword"),
+        "HOST": 'db',
+        "PORT": '5432',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,7 @@ import CalorieCalc from './sites/CalorieCalc';
 
 function App() {
   // constants
-  const base_url = 'http://127.0.0.1:8000/'
+  const base_url = 'http://139.162.147.158:8000/'
   return (
     <div>
       <Navbar />
